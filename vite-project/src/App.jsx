f27ab469d4b2675527110c9ab455db4a92d15e38
grbzemir <eyetkingurbuz@trakya.edu.tr>
2024-10-07 23:18:24 +0300

    Table.jsx

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,7 +1,5 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from 'react';
+import './App.css';
 import Table from "./Table";
 
 function App() {
@@ -12,64 +10,78 @@ function App() {
       surname: 'Gürbüz',
       email: 'emircangrz@gmail.com',
       age: '21'
-
     },
     {
       name: 'Eren Efe',
       surname: 'Yıldız',
       email: 'erenefeyldz@gmail.com',
       age: '21'
-
     },
     {
       name: 'Can Tekin',
       surname: 'Tekin',
       email: 'cantkn1@gmail.com',
       age: '21'
-
     },
     {
       name: 'Muharrem',
       surname: 'Kalaycıoğlu',
       email: 'mhrremklycgl@gmail.com',
       age: '21'
-
     },
     {
       name: 'Sena Sare',
       surname: 'Ergin',
       email: 'snaergin@gmail.com',
       age: '21'
-
     },
     {
       name: 'Berra',
       surname: 'Demirağ',
       email: 'berrademirağ1@gmail.com',
       age: '21'
-
     },
     {
       name: 'Rana',
       surname: 'Sevinç',
       email: 'ranasvnc1@gmail.com',
       age: '21'
-
+    },
+    {
+      name: 'Zuhal',
+      surname: 'Kaya',
+      email: 'zuhalkaya@gmail.com',
+      age: '21'
+    },
+    {
+      name: 'Amine Barçın',
+      surname: 'Özadam',
+      email: 'abarçinözadam@gmail.com',
+      age: '24'
     }
   ]
 
-
-
   return (
-    <div className="w-full border rounded p-4">
-      <Table head={['Ad-Soyad', 'E-posta']}
+    <div className="p-4">
+      <Table
+        searchable={true}
+        head={[
+          { name: 'Ad-Soyad', sortable: true },
+          { name: 'E-Posta' },
+          { name: 'Yaş', sortable: true },
+          { name: 'İşlemler', width: 200 },
+        ]}
         body={users.map(user => ([
-          user.name,
+          `${user.name} ${user.surname}`,
           user.email,
           user.age,
+          [
+            <button key={`${user.email}-edit`} className="px-4 py-2 items-center rounded bg-blue-600 text-white">Düzenle</button>,
+            <button key={`${user.email}-delete`} className="px-4 py-2 items-center rounded bg-red-600 text-white">Sil</button>
+          ]
         ]))} />
     </div>
   )
 }
 
-export default App
+export default App;
--- vite-project/src/Table.jsx
+++ vite-project/src/Table.jsx
@@ -1,31 +1,68 @@
 import PropTypes from 'prop-types';
-export default function Table({ head, body }) {
+import React, { useState } from 'react';
+export default function Table({ head, body, searchable }) {
+
+    const [search, setSearch] = useState('')
+    const filteredData = body.filter(
+        items => items.some(item => item.toString().toLocaleLowerCase('TR').includes(search)))
+    const [sorting, setSorting] = useState({})
+
     return (
-        <div className="w-full border rounded p-4">
-            <table>
+        <>
+            {/* <pre>{JSON.stringify(filteredData, null, 2)}</pre> */}
+            {/* <div className="w-full border rounded p-4"> */}
+            {searchable && (
+                <div className="mb-4">
+                    <input value={search}
+                        onChange={e => setSearch(e.target.value)}
+                        type="text"
+                        className="h-10 outline-none focus:border-black border rounded text-sm px-4 w-full border-gray-300" placeholder="Tabloda Ara..."></input>
+                </div>
+            )}
+            <table className="w-full">
                 <thead>
                     <tr>
                         {head.map((h, key) => (
-                            <th className="text-left , text-sm font-semibold text-gray-500 p-4"
-                                key={key}>{h}</th>
+                            <th
+                                width={h?.width}
+                                className="text-left bg-gray-50 text-sm font-semibold text-gray-500 p-3 border-b"
+                                key={key}>
+                                {h.name}
+                                {h.sortable &&
+                                    (
+                                        <button disabled>Sortable</button>
+                                    )}
+                            </th>
                         ))}
                     </tr>
                 </thead>
                 <tbody>
-                    {body.map((items, key) => (
-                        <tr key={key}>
+                    {filteredData.map((items, key) => (
+                        <tr className="group" key={key}>
                             {items.map((item, key) => (
-                                <td key={key}>{item}</td>
+                                <td
+                                    className="p-3 text-sm
+                                 group-hover:bg-blue-50
+                                  group-hover:text-blue-600"
+                                    key={key}>
+                                    {Array.isArray(item) ? (
+                                        <div className="flex gap-x-2.5 ">
+                                            {item}
+                                        </div>
+                                    ) : item}
+                                </td>
                             ))}
                         </tr>
                     ))}
                 </tbody>
             </table>
-        </div>
+            {/* </div> */}
+        </>
     )
 }
 
 Table.propTypes = {
     head: PropTypes.array.isRequired,
-    body: PropTypes.array.isRequired
+    body: PropTypes.array.isRequired,
+    searchable: PropTypes.bool
 };
